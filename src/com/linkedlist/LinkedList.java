b8cabca8352e91af088d0a03d7c05a8b78718389
Nishant Kumar <nishantkumarkd@gmail.com>
2022-07-01 09:19:28 +0530

    Creating a basic linked list

--- src/com/linkedlist/LinkedList.java
+++ src/com/linkedlist/LinkedList.java
@@ -2,6 +2,21 @@ package com.linkedlist;
 
 public class LinkedList {
     public static void main(String[] args) {
-        System.out.println("Welcome to Data Structure, we will be working with Linked List.");
+    System.out.println("Welcome to Data Structures, We will be working with Linked List");
+    Node first=new Node(56);
+    Node second=new Node(30);
+    Node third=new Node(70);
+
+    Node head=first;
+    first.next=second;
+    second.next=third;
+    Node tail =third;
+
+    Node temp = head;
+    while(temp!=null){
+        System.out.println(temp.data);
+        temp =temp.next;
     }
+
+}
 }
