a48e17ad85d5adbe1efd5b86f453ffb758c0d95c
Nishan Kumar <nishantkumarkd@gmail.com>
2022-07-01 18:58:21 +0530

    Inserting new node between existing nodes

--- src/com/linkedlist/Entry.java
+++ src/com/linkedlist/Entry.java
@@ -2,7 +2,7 @@ package com.linkedlist;
 
 public class Entry {
     public static void main(String[] args) {
-        System.out.println("\nWelcome to Data Structures, we will be working with Linked List, our program begins here.\n");
+        System.out.println("\nWelcome to Data Structures, we will be working with Linked List, our program starts here.\n");
         Operations.pushAtStart();
         Operations.appendData();
         Operations.insertInBetween();
